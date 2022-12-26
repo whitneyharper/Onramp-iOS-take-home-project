50dc2d3687ba5a68b6f946d0dbd5523834e5fdfb
whitneyharper <whitneyharper1@aol.com>
2022-12-26 12:34:26 -0500

    Initial Commit

---
+++ RandomQuoteApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomQuoteApp
+//
+//  Created by Whitney Harper on 12/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
