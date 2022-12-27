2ba2efe1a3d3bb1304e02b3b27a8103ca5797d97
whitneyharper <whitneyharper1@aol.com>
2022-12-26 19:21:09 -0500

    Added Quote model and Model folder. Created fetchQuote function and have button to call it when clicked. Method makes call to api. Finally have some default text for view load.

---
+++ RandomQuoteApp/Model/Quote.swift
@@ -0,0 +1,14 @@
+//
+//  Quote.swift
+//  RandomQuoteApp
+//
+//  Created by Whitney Harper on 12/26/22.
+//
+
+import Foundation
+
+struct Quote: Codable {
+    var id: Int
+    var quote: String
+    var author: String
+}
